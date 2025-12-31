3978843f0d2c106a52a44e75019dc2244c7b6982
Mia <amelia.decobert@gmail.com>
2025-12-31 14:55:16 +0100

    create public folder with reset css, if needed, and first test; refactoring

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 // Import and config dotenv to manage environment variables
 import "dotenv/config";
 // Import router
-import router from "./src/routers/router.js";
+import router from "./routers/router.js";
 
 // Set up PORT and create express instance
 const PORT = process.env.PORT || 3000;
@@ -13,6 +13,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
+app.use(express.static("../public"));
 app.use(express.json());
 app.use(router);
 
